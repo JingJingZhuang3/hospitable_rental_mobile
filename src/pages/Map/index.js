1ae59b282448e8c2277bde7513e0fbf8ac098413
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-10-22 21:14:07 -0400

    Able to change map location based on current selected city. Added zoom and scale control to map.

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -4,17 +4,36 @@ import styles from './index.module.css'
 
 export default class CityMap extends React.Component{
     componentDidMount() {
+        this.initMap()
+    }
+
+    initMap() {
         /**
          * Notes: 
          * In react scaffolding, global objects need to be accessed using Windows, 
          * otherwise will cuz ESLint error
          */
+        // get current located city
+        const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'))
+        console.log(label, value)
         const map = new window.BMapGL.Map('container')
-        // set a center point
-        const point = new window.BMapGL.Point(116.404, 39.915)
-        // Initialize the Map
-        map.centerAndZoom(point, 15)
+        // Created an address parsing variable
+        var myGeo = new window.BMapGL.Geocoder();
+        // parse city name to lng and lat
+        myGeo.getPoint(label, function(point){
+            if (point) {
+                // Initialize the Map
+                map.centerAndZoom(point, 11);
+                // Added scale control
+                map.addControl(new window.BMapGL.ScaleControl());
+                // Added zoom control
+                map.addControl(new window.BMapGL.ZoomControl());
+            }else{
+                alert('The address you selected was unable to parsed!');
+            }
+        }, label)
     }
+
     render() {
         return (
             <div className={styles.map}>
